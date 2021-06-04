bef8e35c76e866ebe59cb1226394f3ddfaaeb7ae
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-06-04 18:41:37 -0300

    Adicionado redirect do usuário ao clicar no logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,10 @@
+import Link from 'next/link';
+
 export default function Header() {
   // TODO
-  return <img src="/Logo.png" alt="logo" />;
+  return (
+    <Link href={'/'}>
+      <img src="/Logo.png" alt="logo" style={{ cursor: 'pointer' }} />
+    </Link>
+  );
 }
